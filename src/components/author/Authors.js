f2ae1a0c5463fc6630e6e25ab43ecb11e45ec562
Roya <foroughan.royaa@gmail.com>
2024-04-20 00:00:28 +0330

    show authors

--- src/components/author/Authors.js
+++ src/components/author/Authors.js
@@ -1,8 +1,31 @@
+import { useQuery } from '@apollo/client'
 import React from 'react'
+import { GET_AUTHORS_INFO } from '../../graphql/queries'
+import { Avatar, Grid, Typography } from '@mui/material'
 
 function Authors() {
+  const {loading , data , errors} = useQuery(GET_AUTHORS_INFO)
+  
+  if(loading) return <h3>Loading...</h3>
+
+  if(errors) return <h3>Error...</h3>
+
+  console.log(data)
+  const {authors} = data
   return (
-    <div>Authors</div>
+    <Grid container sx={{ boxShadow: 'rgba(0,0,0,0.1) 0px 4px 12px' , borderRadius:4 }}
+    >
+      {authors.map(author => (
+        <Grid item xs={12} padding={2}>
+          <a href={`/authors/${author.slug}`}
+          style={{display:'flex' , alignItems:'center' , textDecoration:'none'}}
+          >
+          <Avatar src={author.avatar.url} sx={{marginLeft : 2}}/>
+          <Typography component='p' variant='p' color='text.secondary'>{author.name}</Typography>
+          </a>
+        </Grid>
+      ))}
+    </Grid>
   )
 }
 
--- src/components/shared/CardEL.js
+++ src/components/shared/CardEL.js
@@ -1,12 +1,12 @@
 import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Divider, Typography } from '@mui/material'
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 function CardEL({title , slug , coverPhoto , authors}) {
 
   return (
     
-    <Card sx={{boxShadow: 'rgba(0,0,0,0.1) 0px 4px 12px' , borderRadius:4 , minHeight:'500'}}>
+    <Card sx={{ boxShadow: 'rgba(0,0,0,0.1) 0px 4px 12px' , borderRadius:4 }}>
         <CardHeader 
         avatar={<Avatar src={authors.avatar.url} sx={{marginLeft:2}}/>}
         title={<Typography component='p' variant='p' color='text.secondary' fontWeight={600}>
@@ -14,7 +14,10 @@ function CardEL({title , slug , coverPhoto , authors}) {
         </Typography>}
         />
         <CardMedia component='img' height='194' image={coverPhoto.url} alt={slug}/>
-        <CardContent component='h3' variant='h6' color='text.primary'>{title}</CardContent>
+        <CardContent  component='h6' variant='h6' color='text.primary' 
+        >
+            {title}
+        </CardContent>
         <Divider variant='middle' sx={{margin:'10px'}}/>
         <CardActions>
             {/* <Link to={`/blogs/${slug}`} style={{textDecoration:'none' , width:'100%'}}> */}
